fa590d73d0eb568f45b7ce6cf2dcc927d7e3cc67
paulocostasp <114574459+paulocostasp@users.noreply.github.com>
2023-01-09 11:39:12 -0300

    Update texture.h

--- ProjetoFinal/texture.h
+++ ProjetoFinal/texture.h
@@ -6,75 +6,74 @@ typedef struct BMPImagem
     int   width;
     int   height;
     char *data;
-}BMPImage;
+}BMP;
 
 GLuint texture_id[2];
 
-void getBitmapImageData( char *pFileName, BMPImage *pImage ){
-    FILE *pFile = NULL;
-    unsigned short nNumPlanes;
-    unsigned short nNumBPP;
-	int i;
+void getBitmapImageData( char *fileName, BMP *Image ){
+    FILE *image = NULL;
+    unsigned short numPlanes;
+    unsigned short numBPR;
+	int a;
 
-    if( (pFile = fopen(pFileName, "rb") ) == NULL )
-		printf("ERROR: getBitmapImageData - %s not found.\n", pFileName);
-
+    if((image = fopen(fileName, "rb")) == NULL){
+		printf("ERROR: getBitmapImageData - %s not found.\n",fileName);
+    }
     // Seek forward to width and height info
-    fseek( pFile, 18, SEEK_CUR );
+    fseek(image,18,SEEK_CUR);
 
-    if( (i = fread(&pImage->width, 4, 1, pFile) ) != 1 )
-		printf("ERROR: getBitmapImageData - Couldn't read width from %s.\n ", pFileName);
+    if((a = fread(&Image->width,4,1,image)) != 1){
+		printf("ERROR: getBitmapImageData - Couldn't read width from %s.\n",fileName);
+    }
 
-    if( (i = fread(&pImage->height, 4, 1, pFile) ) != 1 )
-		printf("ERROR: getBitmapImageData - Couldn't read height from %s.\n ", pFileName);
+    if((a = fread(&Image->height,4,1,image)) != 1){
+		printf("ERROR: getBitmapImageData - Couldn't read height from %s.\n",fileName);
+    }
 
-    if( (fread(&nNumPlanes, 2, 1, pFile) ) != 1 )
-		printf("ERROR: getBitmapImageData - Couldn't read plane count from %s.\n", pFileName);
+    if((fread(&numPlanes,2,1,image)) != 1){
+		printf("ERROR: getBitmapImageData - Couldn't read plane count from %s.\n",fileName);
+    }
 
-    if( nNumPlanes != 1 )
-		printf("ERROR: getBitmapImageData - Plane count from %s.\n ", pFileName);
+    if(numPlanes != 1){
+		printf("ERROR: getBitmapImageData - Plane count from %s.\n",fileName);
+    }
 
-    if( (i = fread(&nNumBPP, 2, 1, pFile)) != 1 )
-		printf( "ERROR: getBitmapImageData - Couldn't read BPP from %s.\n ", pFileName);
+    if((a = fread(&numBPR,2,1,image)) != 1){
+		printf("ERROR: getBitmapImageData - Couldn't read BPP from %s.\n",fileName);
+    }
 
-    if( nNumBPP != 24 )
-		printf("ERROR: getBitmapImageData - BPP from %s.\n ", pFileName);
-
+    if(numBPR != 24){
+		printf("ERROR: getBitmapImageData - BPP from %s.\n ",fileName);
+    }
     // Seek forward to image data
-    fseek( pFile, 24, SEEK_CUR );
-
+    fseek(image,24,SEEK_CUR);
 	// Calculate the image's total size in bytes. Note how we multiply the
 	// result of (width * height) by 3. This is becuase a 24 bit color BMP
 	// file will give you 3 bytes per pixel.
-    int nTotalImagesize = (pImage->width * pImage->height) * 3;
+    int nTotalImagesize = (Image->width*Image->height)*3;
 
-    pImage->data = (char*) malloc( nTotalImagesize );
+    Image->data = (char*) malloc(nTotalImagesize);
 
-    if( (i = fread(pImage->data, nTotalImagesize, 1, pFile) ) != 1 )
-		printf("ERROR: getBitmapImageData - Couldn't read image data from %s.\n ", pFileName);
-
-    //
+    if((a = fread(Image->data,nTotalImagesize,1,image)) != 1)
+		printf("ERROR: getBitmapImageData - Couldn't read image data from %s.\n",fileName);
 	// Finally, rearrange BGR to RGB
-	//
-
 	char charTemp;
-    for( i = 0; i < nTotalImagesize; i += 3 )
-	{
-		charTemp = pImage->data[i];
-		pImage->data[i] = pImage->data[i+2];
-		pImage->data[i+2] = charTemp;
+    for(a = 0; a < nTotalImagesize;a += 3){
+		charTemp = Image->data[a];
+		Image->data[a] = Image->data[a+2];
+		Image->data[a+2] = charTemp;
     }
 }
 
 
 /*Função para Carregar uma imagem .BMP */
-void CarregaTextura(char* Filename, int current_texture){
+void chargeTextura(char* Filename, int current_texture){
 
-    BMPImage textura;
+    BMP c_texture;
 
     glBindTexture(GL_TEXTURE_2D,texture_id[current_texture]);
 
-    getBitmapImageData( Filename, &textura);
+    getBitmapImageData( Filename, &c_texture);
 
     // Define os parâmetros da textura
     glBindTexture(GL_TEXTURE_2D,texture_id[current_texture]);
@@ -86,7 +85,7 @@ void CarregaTextura(char* Filename, int current_texture){
 
     // Give the image to OpenGL
     // Define uma textura bidimensional
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, textura.width, textura.height, 0, GL_RGB, GL_UNSIGNED_BYTE, textura.data);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, c_texture.width, c_texture.height, 0, GL_RGB, GL_UNSIGNED_BYTE, c_texture.data);
     glEnable(GL_TEXTURE_2D);
 }
 
@@ -94,9 +93,8 @@ void CarregaTextura(char* Filename, int current_texture){
 /* **********************************************************************
   void init(void)
 		Inicializa os par‚metros globais de OpenGL
-
  ********************************************************************** */
-void init(int textures) {
+void init_texture(int textures) {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);			/* Black Background */
 	glEnable(GL_DEPTH_TEST);
          glGenTextures(1,&texture_id[textures]);
